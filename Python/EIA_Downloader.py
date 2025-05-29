63856b3df0c4c087d61bddda3e43e10312072970
renald_e <edoardo.renaldin@psi.ch>
2025-05-29 14:49:30 +0200

    Update store max MWh

--- Python/EIA_Downloader.py
+++ Python/EIA_Downloader.py
@@ -37,6 +37,30 @@ def URL_constructor(
         f'end={last_hour}&sort[0][column]=period&sort[0][direction]=asc&' + \
         f'offset={offset}&' + \
         f'length={length_query}'
+        
+def get_max_watthour(df, energy_source):
+    ''' Find the maximum watthours for each year ''' 
+    
+    # Get the first energy date
+    start_dt = df['date'].iloc[0]
+    end_dt = df['date'].iloc[-1]
+    
+    # Difference in year between start and end datetime of EIA data
+    no_years = int((end_dt - start_dt).total_seconds() / (365 * 24 * 3600))
+    
+    # Initialize list of max watthours
+    max_watthour_lst = []
+    
+    # Temp variable to set date boundaries
+    previous_year_dt = start_dt
+    for i in range(no_years):
+        next_year_dt = start_dt + dt.timedelta(years = i + 1)
+        
+        mask = (df['date'] >= previous_year_dt) & (df['date'] < next_year_dt)
+        
+        max_watthour_lst.append(df[energy_source][mask].max())
+    
+    return max_watthour_lst
 
 def get_energy_df_from_api(
         first_hour_dt: dt.datetime,
@@ -118,6 +142,9 @@ def get_energy_df_from_api(
         
         # Update number of datetimes downloaded so far
         current += length_query
+        
+    # Store max watthours
+    max_watthours = get_max_watthour(df, energy_source)
     
     # Time the process
     delta_time = (dt.datetime.today() - start).total_seconds() / 60
@@ -125,7 +152,7 @@ def get_energy_df_from_api(
     # Update on downloaded dataset
     print(energy_source + f' -- finished download in {delta_time:.2f} minute(s)')
     
-    return df
+    return df, max_watthours
 
 def clean_energy(master_df):
     """
@@ -219,7 +246,7 @@ def main(region_dict: dict[str]) -> None:
             if energy_source_dictionary[api_energy_code]['In_EIA']:
                 
                 # Get the megawatts produced hourly
-                energy_df = get_energy_df_from_api(
+                energy_df, max_watthour = get_energy_df_from_api(
                     first_hour_dt,
                     last_hour_dt,
                     total_num_records,
@@ -264,137 +291,137 @@ def main(region_dict: dict[str]) -> None:
 # call the main method on each region with their associated list of available energy
 region_dict = {
 
-    'CAL':{
-        'SUN':{'Title' :'Solar','In_EIA':True},
-        'NUC':{'Title' :'Nuclear','In_EIA':True},
-        'WND':{'Title' :'Wind','In_EIA':True},
-        'COL':{'Title' :'Coal','In_EIA':True},
-        'WAT':{'Title' :'Hydro','In_EIA':True},
-        'NG': {'Title' :'Gas','In_EIA':True},
-        'OTH':{'Title' :'Other','In_EIA':True},
-        'OIL':{'Title' :'Oil','In_EIA':True}
-        },  
+    # 'CAL':{
+    #     'SUN':{'Title' :'Solar','In_EIA':True},
+    #     'NUC':{'Title' :'Nuclear','In_EIA':True},
+    #     'WND':{'Title' :'Wind','In_EIA':True},
+    #     'COL':{'Title' :'Coal','In_EIA':True},
+    #     'WAT':{'Title' :'Hydro','In_EIA':True},
+    #     'NG': {'Title' :'Gas','In_EIA':True},
+    #     'OTH':{'Title' :'Other','In_EIA':True},
+    #     'OIL':{'Title' :'Oil','In_EIA':True}
+    #     },  
  
-    'CAR':{
-        'SUN':{'Title' :'Solar','In_EIA':True},
-        'NUC':{'Title' :'Nuclear','In_EIA':True},
-        'WND':{'Title' :'Wind','In_EIA':False},
-        'COL':{'Title' :'Coal','In_EIA':True},
-        'WAT':{'Title' :'Hydro','In_EIA':True},
-        'NG': {'Title' :'Gas','In_EIA':True},
-        'OTH':{'Title' :'Other','In_EIA':True},
-        'OIL':{'Title' :'Oil','In_EIA':True}
-        },
+    # 'CAR':{
+    #     'SUN':{'Title' :'Solar','In_EIA':True},
+    #     'NUC':{'Title' :'Nuclear','In_EIA':True},
+    #     'WND':{'Title' :'Wind','In_EIA':False},
+    #     'COL':{'Title' :'Coal','In_EIA':True},
+    #     'WAT':{'Title' :'Hydro','In_EIA':True},
+    #     'NG': {'Title' :'Gas','In_EIA':True},
+    #     'OTH':{'Title' :'Other','In_EIA':True},
+    #     'OIL':{'Title' :'Oil','In_EIA':True}
+    #     },
  
-    'CENT':{
-        'SUN':{'Title' :'Solar','In_EIA':True},
-        'NUC':{'Title' :'Nuclear','In_EIA':True},
-        'WND':{'Title' :'Wind','In_EIA':True},
-        'COL':{'Title' :'Coal','In_EIA':True},
-        'WAT':{'Title' :'Hydro','In_EIA':True},
-        'NG': {'Title' :'Gas','In_EIA':True},
-        'OTH':{'Title' :'Other','In_EIA':True},
-        'OIL':{'Title' :'Oil','In_EIA':True}
-        },
+    # 'CENT':{
+    #     'SUN':{'Title' :'Solar','In_EIA':True},
+    #     'NUC':{'Title' :'Nuclear','In_EIA':True},
+    #     'WND':{'Title' :'Wind','In_EIA':True},
+    #     'COL':{'Title' :'Coal','In_EIA':True},
+    #     'WAT':{'Title' :'Hydro','In_EIA':True},
+    #     'NG': {'Title' :'Gas','In_EIA':True},
+    #     'OTH':{'Title' :'Other','In_EIA':True},
+    #     'OIL':{'Title' :'Oil','In_EIA':True}
+    #     },
  
-    'FLA':{
-        'SUN':{'Title' :'Solar','In_EIA':True},
-        'NUC':{'Title' :'Nuclear','In_EIA':True},
-        'WND':{'Title' :'Wind','In_EIA':False},
-        'COL':{'Title' :'Coal','In_EIA':True},
-        'WAT':{'Title' :'Hydro','In_EIA':True},
-        'NG': {'Title' :'Gas','In_EIA':True},
-        'OTH':{'Title' :'Other','In_EIA':True},
-        'OIL':{'Title' :'Oil','In_EIA':True}
-        },
+    # 'FLA':{
+    #     'SUN':{'Title' :'Solar','In_EIA':True},
+    #     'NUC':{'Title' :'Nuclear','In_EIA':True},
+    #     'WND':{'Title' :'Wind','In_EIA':False},
+    #     'COL':{'Title' :'Coal','In_EIA':True},
+    #     'WAT':{'Title' :'Hydro','In_EIA':True},
+    #     'NG': {'Title' :'Gas','In_EIA':True},
+    #     'OTH':{'Title' :'Other','In_EIA':True},
+    #     'OIL':{'Title' :'Oil','In_EIA':True}
+    #     },
 
-    'MIDA':{
-        'SUN':{'Title' :'Solar','In_EIA':True},
-        'NUC':{'Title' :'Nuclear','In_EIA':True},
-        'WND':{'Title' :'Wind','In_EIA':True},
-        'COL':{'Title' :'Coal','In_EIA':True},
-        'WAT':{'Title' :'Hydro','In_EIA':True},
-        'NG': {'Title' :'Gas','In_EIA':True},
-        'OTH':{'Title' :'Other','In_EIA':True},
-        'OIL':{'Title' :'Oil','In_EIA':True}
-        },
+    # 'MIDA':{
+    #     'SUN':{'Title' :'Solar','In_EIA':True},
+    #     'NUC':{'Title' :'Nuclear','In_EIA':True},
+    #     'WND':{'Title' :'Wind','In_EIA':True},
+    #     'COL':{'Title' :'Coal','In_EIA':True},
+    #     'WAT':{'Title' :'Hydro','In_EIA':True},
+    #     'NG': {'Title' :'Gas','In_EIA':True},
+    #     'OTH':{'Title' :'Other','In_EIA':True},
+    #     'OIL':{'Title' :'Oil','In_EIA':True}
+    #     },
 
-    'MIDW':{
-        'SUN':{'Title' :'Solar','In_EIA':True},
-        'NUC':{'Title' :'Nuclear','In_EIA':True},
-        'WND':{'Title' :'Wind','In_EIA':True},
-        'COL':{'Title' :'Coal','In_EIA':True},
-        'WAT':{'Title' :'Hydro','In_EIA':True},
-        'NG': {'Title' :'Gas','In_EIA':True},
-        'OTH':{'Title' :'Other','In_EIA':True},
-        'OIL':{'Title' :'Oil','In_EIA':False}
-        },
+    # 'MIDW':{
+    #     'SUN':{'Title' :'Solar','In_EIA':True},
+    #     'NUC':{'Title' :'Nuclear','In_EIA':True},
+    #     'WND':{'Title' :'Wind','In_EIA':True},
+    #     'COL':{'Title' :'Coal','In_EIA':True},
+    #     'WAT':{'Title' :'Hydro','In_EIA':True},
+    #     'NG': {'Title' :'Gas','In_EIA':True},
+    #     'OTH':{'Title' :'Other','In_EIA':True},
+    #     'OIL':{'Title' :'Oil','In_EIA':False}
+    #     },
     
-    'NE':{
-        'SUN':{'Title' :'Solar','In_EIA':True},
-        'NUC':{'Title' :'Nuclear','In_EIA':True},
-        'WND':{'Title' :'Wind','In_EIA':True},
-        'COL':{'Title' :'Coal','In_EIA':True},
-        'WAT':{'Title' :'Hydro','In_EIA':True},
-        'NG': {'Title' :'Gas','In_EIA':True},
-        'OTH':{'Title' :'Other','In_EIA':True},
-        'OIL':{'Title' :'Oil','In_EIA':True}
-        },
+    # 'NE':{
+    #     'SUN':{'Title' :'Solar','In_EIA':True},
+    #     'NUC':{'Title' :'Nuclear','In_EIA':True},
+    #     'WND':{'Title' :'Wind','In_EIA':True},
+    #     'COL':{'Title' :'Coal','In_EIA':True},
+    #     'WAT':{'Title' :'Hydro','In_EIA':True},
+    #     'NG': {'Title' :'Gas','In_EIA':True},
+    #     'OTH':{'Title' :'Other','In_EIA':True},
+    #     'OIL':{'Title' :'Oil','In_EIA':True}
+    #     },
     
-    'NW':{
-        'SUN':{'Title' :'Solar','In_EIA':True},
-        'NUC':{'Title' :'Nuclear','In_EIA':True},
-        'WND':{'Title' :'Wind','In_EIA':True},
-        'COL':{'Title' :'Coal','In_EIA':True},
-        'WAT':{'Title' :'Hydro','In_EIA':True},
-        'NG': {'Title' :'Gas','In_EIA':True},
-        'OTH':{'Title' :'Other','In_EIA':True},
-        'OIL':{'Title' :'Oil','In_EIA':True}
-        },
+    # 'NW':{
+    #     'SUN':{'Title' :'Solar','In_EIA':True},
+    #     'NUC':{'Title' :'Nuclear','In_EIA':True},
+    #     'WND':{'Title' :'Wind','In_EIA':True},
+    #     'COL':{'Title' :'Coal','In_EIA':True},
+    #     'WAT':{'Title' :'Hydro','In_EIA':True},
+    #     'NG': {'Title' :'Gas','In_EIA':True},
+    #     'OTH':{'Title' :'Other','In_EIA':True},
+    #     'OIL':{'Title' :'Oil','In_EIA':True}
+    #     },
     
-    'NY':{
-        'SUN':{'Title' :'Solar','In_EIA':True},
-        'NUC':{'Title' :'Nuclear','In_EIA':True},
-        'WND':{'Title' :'Wind','In_EIA':True},
-        'COL':{'Title' :'Coal','In_EIA':True},
-        'WAT':{'Title' :'Hydro','In_EIA':True},
-        'NG': {'Title' :'Gas','In_EIA':True},
-        'OTH':{'Title' :'Other','In_EIA':True},
-        'OIL':{'Title' :'Oil','In_EIA':True}
-        },
+    # 'NY':{
+    #     'SUN':{'Title' :'Solar','In_EIA':True},
+    #     'NUC':{'Title' :'Nuclear','In_EIA':True},
+    #     'WND':{'Title' :'Wind','In_EIA':True},
+    #     'COL':{'Title' :'Coal','In_EIA':True},
+    #     'WAT':{'Title' :'Hydro','In_EIA':True},
+    #     'NG': {'Title' :'Gas','In_EIA':True},
+    #     'OTH':{'Title' :'Other','In_EIA':True},
+    #     'OIL':{'Title' :'Oil','In_EIA':True}
+    #     },
     
-    'SE':{
-        'SUN':{'Title' :'Solar','In_EIA':True},
-        'NUC':{'Title' :'Nuclear','In_EIA':True},
-        'WND':{'Title' :'Wind','In_EIA':True},
-        'COL':{'Title' :'Coal','In_EIA':True},
-        'WAT':{'Title' :'Hydro','In_EIA':True},
-        'NG': {'Title' :'Gas','In_EIA':True},
-        'OTH':{'Title' :'Other','In_EIA':True},
-        'OIL':{'Title' :'Oil','In_EIA':True}
-        },
+    # 'SE':{
+    #     'SUN':{'Title' :'Solar','In_EIA':True},
+    #     'NUC':{'Title' :'Nuclear','In_EIA':True},
+    #     'WND':{'Title' :'Wind','In_EIA':True},
+    #     'COL':{'Title' :'Coal','In_EIA':True},
+    #     'WAT':{'Title' :'Hydro','In_EIA':True},
+    #     'NG': {'Title' :'Gas','In_EIA':True},
+    #     'OTH':{'Title' :'Other','In_EIA':True},
+    #     'OIL':{'Title' :'Oil','In_EIA':True}
+    #     },
     
-    'SW':{
-        'SUN':{'Title' :'Solar','In_EIA':True},
-        'NUC':{'Title' :'Nuclear','In_EIA':True},
-        'WND':{'Title' :'Wind','In_EIA':True},
-        'COL':{'Title' :'Coal','In_EIA':True},
-        'WAT':{'Title' :'Hydro','In_EIA':True},
-        'NG': {'Title' :'Gas','In_EIA':True},
-        'OTH':{'Title' :'Other','In_EIA':True},
-        'OIL':{'Title' :'Oil','In_EIA':True}
-        },
+    # 'SW':{
+    #     'SUN':{'Title' :'Solar','In_EIA':True},
+    #     'NUC':{'Title' :'Nuclear','In_EIA':True},
+    #     'WND':{'Title' :'Wind','In_EIA':True},
+    #     'COL':{'Title' :'Coal','In_EIA':True},
+    #     'WAT':{'Title' :'Hydro','In_EIA':True},
+    #     'NG': {'Title' :'Gas','In_EIA':True},
+    #     'OTH':{'Title' :'Other','In_EIA':True},
+    #     'OIL':{'Title' :'Oil','In_EIA':True}
+    #     },
     
-    'TEN':{
-        'SUN':{'Title' :'Solar','In_EIA':True},
-        'NUC':{'Title' :'Nuclear','In_EIA':True},
-        'WND':{'Title' :'Wind','In_EIA':True},
-        'COL':{'Title' :'Coal','In_EIA':True},
-        'WAT':{'Title' :'Hydro','In_EIA':True},
-        'NG': {'Title' :'Gas','In_EIA':True},
-        'OTH':{'Title' :'Other','In_EIA':True},
-        'OIL':{'Title' :'Oil','In_EIA':True}
-        },
+    # 'TEN':{
+    #     'SUN':{'Title' :'Solar','In_EIA':True},
+    #     'NUC':{'Title' :'Nuclear','In_EIA':True},
+    #     'WND':{'Title' :'Wind','In_EIA':True},
+    #     'COL':{'Title' :'Coal','In_EIA':True},
+    #     'WAT':{'Title' :'Hydro','In_EIA':True},
+    #     'NG': {'Title' :'Gas','In_EIA':True},
+    #     'OTH':{'Title' :'Other','In_EIA':True},
+    #     'OIL':{'Title' :'Oil','In_EIA':True}
+    #     },
     
     'TEX':{
         'SUN':{'Title' :'Solar','In_EIA':True},
